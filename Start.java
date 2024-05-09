d3568baeab47b1b96bfb986de111e9ac5f5eb90a
Md Rijoan Maruf <78620963+mdrijoanmaruf@users.noreply.github.com>
2024-05-10 06:35:08 +0900

    Add Portal Profile code.
    Add Portal logout and profile frame.

---
+++ Start.java
@@ -0,0 +1,8 @@
+import portal.*;
+import website.*;
+public class Start {
+    public static void main(String[] args) {
+        ShawonPortal s1 = new ShawonPortal();
+        s1.setVisible(true);
+    }
+}
